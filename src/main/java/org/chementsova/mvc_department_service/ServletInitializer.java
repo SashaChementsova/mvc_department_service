c1d47a64a69ba5976a680a2095bfd2e7f5f3d95f
SashaChementsova <chementsova2102@gmail.com>
2024-08-26 15:36:16 +0300

    Initial commit

---
+++ src/main/java/org/chementsova/mvc_department_service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.chementsova.mvc_department_service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DepartmentServiceApplication.class);
+    }
+
+}
---
+++ src/test/java/org/chementsova/mvc_department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chementsova.mvc_department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
